1ef33fc936b3883818491a4014874e9181a1b267
GouthamBabu <43052583+GouthamBabu@users.noreply.github.com>
2021-06-21 15:55:46 -0400

    Add files via upload

---
+++ Directions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPI
+{
+    public class Directions
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string TotalDuration { get; set; }
+        public string TotalDistance { get; set; }
+        public List<string> Route { get; set; }
+        public string? MapURL { get; set; }
+
+    }
+}
---
+++ Geocode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Geocode
+    {
+        public string FormattedAddress { get; set; }
+        public Geometry Geometry { get; set; }
+        public bool PartialMatch { get; set; }
+        public string PlaceId { get; set; }
+    }
+    public class Geometry
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
