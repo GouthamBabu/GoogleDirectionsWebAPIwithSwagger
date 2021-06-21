776638ee90cf3344cc96ec880508fdc408e0e175
GouthamBabu <43052583+GouthamBabu@users.noreply.github.com>
2021-06-21 15:54:49 -0400

    Add files via upload

---
+++ DirectionsController.cs
@@ -0,0 +1,46 @@
+﻿using GoogleApi.Entities.Maps.Geocoding.Address.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAPI
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class DirectionsController : ControllerBase
+    {
+        private readonly IDirectionService _directionService;
+        public DirectionsController(IDirectionService directionService)
+        {
+            _directionService = directionService;
+        }
+
+        [Route("/api/directions")]
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string origin, string destination ,bool map)
+        {
+            if (string.IsNullOrEmpty(origin)|string.IsNullOrEmpty(destination))
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var Response = await _directionService.GetDirectionsAsync(origin, destination, map);
+                if (Response is null)
+                {
+                    return StatusCode(404);
+                }
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
---
+++ GeocodeController.cs
@@ -0,0 +1,41 @@
+﻿using GoogleApi.Entities.Maps.Geocoding.Address.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAPI
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class GeocodeController : ControllerBase
+    {
+        private readonly IGeocodeService _GeocodeService;
+        public GeocodeController(IGeocodeService geocodeService)
+        {
+            _GeocodeService = geocodeService;
+        }
+
+        [Route("/api/geocode")]
+        [HttpPost]
+        public async Task<IActionResult> Get([FromBody] string address)
+        {
+            if (string.IsNullOrEmpty(address))
+            {
+                return BadRequest();
+            }
+            try
+            {
+                return Ok(await _GeocodeService.GeocodeAddressAsync(address));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
