08dfa40f131fccc7e8bd024ac67764dfb9dd56ab
GouthamBabu <43052583+GouthamBabu@users.noreply.github.com>
2021-06-21 15:58:17 -0400

    Add files via upload

---
+++ Services/IDirectionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GoogleApi.Entities.Maps.Geocoding.Address.Request;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public interface IDirectionService
+    {
+        public Task<Directions> GetDirectionsAsync(string origin, string destination, bool map);
+    }
+}
---
+++ Services/IGeocodeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GoogleApi.Entities.Maps.Geocoding.Address.Request;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public interface IGeocodeService
+    {
+        public Task<Geocode> GeocodeAddressAsync(string address);
+    }
+}
